df7f6c71715e378803248a280647ac903e829c05
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-03-19 10:03:58 +0530

    updated gitignore

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 def addition (a,b):
     return a+b 
+def subtraction (a,b):
+    return a-b
+def multiplication (a,b):
+    return a*b
+def division (a,b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a/b
